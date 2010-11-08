2392c21c17bae8f51925dfd4761401658f69fc85
James McMinn <James McMinn@306c0d56-3788-c67b-985d-2422301581a3>
2010-11-08 23:55:39 +0000

    Created basic structure for code.

---
+++ TP3/trunk/src/client/net/Networking.java
@@ -0,0 +1,10 @@
+package client.net;
+
+/**
+ * This will list all of the functions which the UI code expects the networking code to implement
+ * @author James McMinn
+ *
+ */
+public interface Networking {
+
+}
