f62c360210efa41d73788b6c7631be73e5b93d30
0800890m <0800890m@306c0d56-3788-c67b-985d-2422301581a3>
2010-11-09 11:21:41 +0000

    Added more files

---
+++ TP3/trunk/src/client/net/ClientConnection.java
@@ -0,0 +1,11 @@
+package client.net;
+
+public class ClientConnection implements Runnable, Networking  {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ TP3/trunk/src/client/ui/GimUI.java
@@ -0,0 +1,11 @@
+package client.ui;
+
+public class GimUI implements Runnable, UserInterface {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
