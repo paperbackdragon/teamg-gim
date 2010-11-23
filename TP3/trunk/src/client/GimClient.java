c9e0395e30bda9cfd8892f70acc9d8ac1fe14150
1003914h <1003914h@306c0d56-3788-c67b-985d-2422301581a3>
2010-11-23 13:50:41 +0000

    Test

--- TP3/trunk/src/client/GimClient.java
+++ TP3/trunk/src/client/GimClient.java
@@ -1,9 +1,36 @@
 package client;
 
-public class GimClient {
-	
-	public static void main(String[] args) {
+import java.awt.*;
+
+import javax.swing.*;
+
+public class GimClient extends JFrame{
+	public GimClient() {
+		setTitle("GIM");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JPanel main = new MainWindow();
+		
+		setContentPane(main);
+		pack();
+		setVisible(true);
 	}
 	
+	class MainWindow extends JPanel {
+		public MainWindow() {
+			
+		}
+		
+		public Dimension getPreferredSize() {
+			return new Dimension(300, 400);
+		}
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new GimClient(); 
+			}
+		});
+	}
 }
