0bf068536fbcd3aab485bd022c6db1f2bdae0ff7
0800902m <0800902m@306c0d56-3788-c67b-985d-2422301581a3>
2010-12-16 17:08:58 +0000

    Almost finished the two interfaces

--- TP3/trunk/src/client/net/NetworkingIn.java
+++ TP3/trunk/src/client/net/NetworkingIn.java
@@ -3,182 +3,334 @@ package client.net;
 import java.util.ArrayList;
 
 public interface NetworkingIn {
-	
+
 	// prelogin
-	
+
 	// :OKAY:;
-	
-	/** The last command was send successfully (this is sent where there is no other response) */
+
+	/**
+	 * The last command was send successfully (this is sent where there is no
+	 * other response)
+	 */
 	void okay();
-	
+
 	// :SERVERSTATUS { USERS | TIME | UPTIME }:;
-	
-	/** The server has sent information about the global status of the server
+
+	/**
+	 * The server has sent information about the global status of the server
+	 * 
 	 * @param status
-	 * 		The string with global information */
+	 *            The string with global information
+	 */
 	void status(String status);
-	
-	/** The server has sent information about the server's time 
+
+	/**
+	 * The server has sent information about the server's time
+	 * 
 	 * @param servertime
-	 * 		The server's time*/
+	 *            The server's time
+	 */
 	void servertime(String servertime);
-	
-	/** The server has sent the number of users connected to the server
+
+	/**
+	 * The server has sent the number of users connected to the server
+	 * 
 	 * @param usercount
-	 * 		The amount of user's connected */
+	 *            The amount of user's connected
+	 */
 	void usercount(String usercount);
-	
-	/** The server has sent information about its uptime
+
+	/**
+	 * The server has sent information about its uptime
+	 * 
 	 * @param uptime
-	 * 		The uptime of the server */
+	 *            The uptime of the server
+	 */
 	void uptime(String uptime);
-	
+
 	// :SERVERSTATUS ends
-	
-	/** The server has indicated that it wants to force the client to disconnect gracefully.
+
+	/**
+	 * The server has indicated that it wants to force the client to disconnect
+	 * gracefully.
+	 * 
 	 * @param message
-	 * 		The reason why the server wants the client to disconnect gracefully. */
+	 *            The reason why the server wants the client to disconnect
+	 *            gracefully.
+	 */
 	void kill(String message);
-	
+
 	// :BROADCAST: <message>;
-	
-	/** The server has sent a global message to all connected user. This is likely to contain critical information
-	 * and should be clearly displayed to the user.
+
+	/**
+	 * The server has sent a global message to all connected user. This is
+	 * likely to contain critical information and should be clearly displayed to
+	 * the user.
+	 * 
 	 * @param message
-	 * 		The message from the server.  */
+	 *            The message from the server.
+	 */
 	void broadcast(String message);
-	
-	
+
 	// :AUTH [ LOGGEDIN | UNAUTHORIZED ]:;
-	
+
 	// NOT SURE ABOUT THESE TWO... WILL TALK TO JAMES
-	
+
 	/** The server indicated the user is authorised */
 	void authorised();
-	
+
 	/** The server indicated the user is unauthorised */
 	void unauthorised();
-	
+
 	// end :AUTH
-	
+
 	// postlogin
-	
-	//:MESSAGE: <roomid> <sender> <message>;
-	
-	/** The server has indicated that the user has received a message
+
+	// :MESSAGE: <roomid> <sender> <message>;
+
+	/**
+	 * The server has indicated that the user has received a message
+	 * 
 	 * @param roomid
-	 * 		The id of the room the message was sent to
+	 *            The id of the room the message was sent to
 	 * @param sender
-	 * 		The person the message was received from
+	 *            The person the message was received from
 	 * @param message
-	 * 		The message the sender sent */
+	 *            The message the sender sent
+	 */
 	void message(String roomid, String sender, String message);
-	
+
 	// need james to clarify on these:
-	
+
 	// :ROOM [ CREATED | JOINED | LEFT | INVITED | USERS ]: <roomid> {<user>};
-	
+
 	void created();
-	
+
 	void joined();
-	
+
 	void left();
-	
+
 	void invited();
-	
+
 	void users();
-	
+
 	// end :ROOM
-	
-	// :p feel free to tell me if you don't think this is a sensible way of dealing with this: 
-	
+
+	// :p feel free to tell me if you don't think this is a sensible way of
+	// dealing with this:
+
 	// :FRIENDLIST: ONLINE <user>{,<user>} OFFLINE <user>{,<user>} BLOCKED
-	
-	/** The server has sent the online status of all the users on the buddy list
+
+	/**
+	 * The server has sent the online status of all the users on the buddy list
+	 * 
 	 * @param onlinelist
-	 * 		An arraylist of the users online on the buddy list
+	 *            An arraylist of the users online on the buddy list
 	 * @param offlinelist
-	 * 		An arraylist of the users offline on the buddy list
+	 *            An arraylist of the users offline on the buddy list
 	 * @param blockedlist
-	 * 		An arraylist of the users who are blocked on the buddy list
-	 *  */
-	
-	void friendlist(ArrayList<String> onlinelist, ArrayList<String> offlinelist, ArrayList<String> blockedlist);
-	
+	 *            An arraylist of the users who are blocked on the buddy list
+	 * */
+
+	void friendlist(ArrayList<String> onlinelist,
+			ArrayList<String> offlinelist, ArrayList<String> blockedlist);
+
 	// :FRIENDREQUEST: <user> <nickname>;
-	
-	/** the server has notified that the user has received a friend request
+
+	/**
+	 * the server has notified that the user has received a friend request
+	 * 
 	 * @param user
-	 * 		The user that has sent a friend request
+	 *            The user that has sent a friend request
 	 * @param nickname
-	 * 		The user's nickname at the time of sending the friend request */
+	 *            The user's nickname at the time of sending the friend request
+	 */
 	void friendrequest(String user, String nickname);
-	
+
 	// :UPDATE [ NICKNAME| STATUS | PERSONAL_MESSAGE | DISPLAY_PIC ]: <user>;
-	
-	/** The server has notified that a user has changed their nickname
+
+	/**
+	 * The server has notified that a user has changed their nickname
+	 * 
 	 * @param user
-	 * 		The user that changed their nickname */
+	 *            The user that changed their nickname
+	 */
 	void notifyNickname(String user);
-	
-	/** The server has notified that a user has changed their status 
+
+	/**
+	 * The server has notified that a user has changed their status
+	 * 
 	 * @param user
-	 * 		The user that has changed their status*/
+	 *            The user that has changed their status
+	 */
 	void notifyStatus(String user);
-	
-	/** The server has notified that a user has changed their personal message
+
+	/**
+	 * The server has notified that a user has changed their personal message
+	 * 
 	 * @param user
-	 * 		The user that has changed their personal message*/
+	 *            The user that has changed their personal message
+	 */
 	void notifyPersonalMessage(String user);
-	
-	/** The server has notified that a user has changed their display picture
+
+	/**
+	 * The server has notified that a user has changed their display picture
+	 * 
 	 * @param user
-	 * 		The user that has changed their display picture*/
+	 *            The user that has changed their display picture
+	 */
 	void notifyDisplayPicture(String user);
-	
+
 	// end :UPDATE
-	
-	
-	// :INFO { NICKNAME | STATUS | PERSONAL_MESSAGE | DISPLAY_PIC }: <user> <data> {<user data>};
-	// ^^ I will parse this and invoke these methods appropriately ... (if we're all agreed this is fine :P)
-	
-	/** The server has given the client information about a user's nickname
-	 * @param user 
-	 * 		The user that the server is giving nickname information about
+
+	// :INFO { NICKNAME | STATUS | PERSONAL_MESSAGE | DISPLAY_PIC }: <user>
+	// <data> {<user data>};
+	// ^^ I will parse this and invoke these methods appropriately ... (if we're
+	// all agreed this is fine :P)
+
+	/**
+	 * The server has given the client information about a user's nickname
+	 * 
+	 * @param user
+	 *            The user that the server is giving nickname information about
 	 * @param nickname
-	 * 		The user's nickname
-	 *  */
+	 *            The user's nickname
+	 * */
 	void updateNickname(String user, String nickname);
-	
-	/** The server has given the client information about a user's status
+
+	/**
+	 * The server has given the client information about a user's status
+	 * 
 	 * @param user
-	 * 		The user that the server is giving status information about
+	 *            The user that the server is giving status information about
 	 * @param status
-	 * 		The user's status */
+	 *            The user's status
+	 */
 	void updateStatus(String user, String status);
-	
-	/** The server has given the client information about a user's personal messages 
+
+	/**
+	 * The server has given the client information about a user's personal
+	 * messages
+	 * 
 	 * @param user
-	 * 		The user that the server is giving personal information about
+	 *            The user that the server is giving personal information about
 	 * @param status
-	 * 		The user's personal message */
+	 *            The user's personal message
+	 */
 	void updatePersonalMessage(String user, String personalmessage);
-	
-	/** The server has given the client information about a user's display picture 
+
+	/**
+	 * The server has given the client information about a user's display
+	 * picture
+	 * 
 	 * @param user
-	 * 		The user that the server is giving display picture information about
+	 *            The user that the server is giving display picture information
+	 *            about
 	 * @param displayPicture
-	 * 		The user's display picture */
+	 *            The user's display picture
+	 */
 	void updateDisplayPicture(String user, String displayPicture);
-	
+
 	// end :INFO
-	
+
 	// errors
-	
-	// Unsure about something here... will speak to james
-	
-	
-	
-	
+
+	/*
+	 * :ERROR [ UNAUTHORISED | INVALID_EMAIL | EMAIL_ALREADY_IN_USE |
+	 * PASSWORD_TOO_SHORT | MISSING_ARGUMENTS | TOO_MANY_ARGUMENTS |
+	 * INVALID_ARGUMENT | LOGGED_IN_FROM_OTHER_LOCATION | USER_DOES_NOT_EXIST |
+	 * LOGIN_DETAILS_INCORRECT ]: <message>;
+	 */
+
+	/**
+	 * Server has thrown an 'unauthorised' error. This is thrown if a user is
+	 * not logged in.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void unauthorisedError(String message);
+
+	/**
+	 * Server has thrown an 'invalid email' error. This is thrown when the sent
+	 * e-mail address is invalid.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void invalidEmailError(String message);
+
+	/**
+	 * Server has thrown an 'E-mail in use error.' This is sent when the user
+	 * tries to register using an e-mail address that is already in use in the
+	 * server
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void emailInuseError(String message);
+
+	/**
+	 * Server has thrown a 'password too short' error. This is thrown when the
+	 * user tries to register with a password that is too short
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void passwordTooShortError(String message);
+
+	/**
+	 * Server has thrown a 'missing argument error.' This is thrown when a
+	 * command is sent to the server and is missing and expected argument.
+	 * 
+	 * @param message
+	 *            message sent the the error. Gives details.
+	 */
+	void missingArgumentsError(String message);
+
+	/**
+	 * Server has thrown a 'too many arguments' error. This is thrown when a
+	 * command is sent with one or more unexpected arguments.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void tooManyArgumentsError(String message);
+
+	/**
+	 * Server has thrown an 'invalid argument' error. This is thrown when an
+	 * argument sent to the server is invalid.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void invalidArgumentError(String message);
+
+	/**
+	 * Server has thrown a 'logged in from another location' error. This occurs
+	 * when the user has attempted to log in in two locations.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void loggedInFromAnotherLocationError(String message);
+
+	/**
+	 * Server has thrown an 'user does not exist' error. This is thrown when the
+	 * uer attempts to log in using an username that does not exist.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void userDoesNotExistError(String message);
+
+	/**
+	 * Server has thrown a 'login details incorrect' error. This is thrown when
+	 * the user has provided incorrect log in details.
+	 * 
+	 * @param message
+	 *            message sent with the error. Gives details.
+	 */
+	void logInDetailsIncorrectError(String message);
 
 }
--- TP3/trunk/src/client/net/NetworkingOut.java
+++ TP3/trunk/src/client/net/NetworkingOut.java
@@ -3,226 +3,305 @@ package client.net;
 import java.util.ArrayList;
 
 /**
- * This will list all of the functions which the UI code expects the networking code to implement, as part of the
- * networking protocol. Although the server's response is out of the scope of this aspect, it will be indicated in
- * the documentation descriptively how the server will respond, as is considered helpful.
+ * This will list all of the functions which the UI code expects the networking
+ * code to implement, as part of the networking protocol. Although the server's
+ * response is out of the scope of this aspect, it will be indicated in the
+ * documentation descriptively how the server will respond, as is considered
+ * helpful.
  * 
  * @author James McMinn
  * @author Gordon Martin
- *
+ * 
  */
 
 public interface NetworkingOut {
-	
+
 	// pre-login commands
-	
+
 	// :PING:;
-	
-	/** used to implement the "keep-alive" command. Sends a ping to the server to 
-	 * check if the client is still "alive" */
+
+	/**
+	 * used to implement the "keep-alive" command. Sends a ping to the server to
+	 * check if the client is still "alive"
+	 */
 	void ping();
-	
-	
+
 	// :SERVERSTATUS { USERS | TIME | UPTIME }:;
-	
-	/** 
-	 * sends a request to the server to return (as a line):
-	 *  the number of users online
-	 *  the time,
-	 *  the uptime of the server */
+
+	/**
+	 * sends a request to the server to return (as a line): the number of users
+	 * online the time, the uptime of the server
+	 */
 	void serverstatus();
-	
+
 	/** sends a request to the server to the number of users logged in */
 	void servernumberofusers();
-	
+
 	/** sends a request to the server to return the server's time */
 	void serverTime();
-	
-	/** sends a request to the server to the server to return the uptime of the server */
+
+	/**
+	 * sends a request to the server to the server to return the uptime of the
+	 * server
+	 */
 	void serverUptime();
-	
+
 	// :SERVERSTATUS ends
-	
+
 	// :AUTH { LOGIN | REGISTER }: <email address> <password>;
-	
-	/** sends a request to the server to log in 
+
+	/**
+	 * sends a request to the server to log in
+	 * 
 	 * @param emailaddress
-	 * 		a valid e-mail address
+	 *            a valid e-mail address
 	 * @param password
-	 * 		the password corresponding to the e-mail address
+	 *            the password corresponding to the e-mail address
 	 * 
-	 *  The server will return the user's authorisation state: logged in, or unauthorised */
+	 *            The server will return the user's authorisation state: logged
+	 *            in, or unauthorised
+	 */
 	void authenticate(String emailaddress, String password);
-	
-	
-	/** sends a request to the server to register an account
-		 * @param emailaddress
-	 * 		the desired e-mail address
-	 * @param password
-	 * 		the desired password
+
+	/**
+	 * sends a request to the server to register an account
 	 * 
-	 *  If the registration is unsuccessful, the server will respond with an error message indicating why */
+	 * @param emailaddress
+	 *            the desired e-mail address
+	 * @param password
+	 *            the desired password
+	 * 
+	 *            If the registration is unsuccessful, the server will respond
+	 *            with an error message indicating why
+	 */
 	void register(String emailaddress, String password);
-	
+
 	// :AUTH ends
-	
+
 	// :QUIT:;
-	
-	/** sends a request to the server to log the user out. 
-	 * If handled successfully, the connection will be terminated from the server. */
+
+	/**
+	 * sends a request to the server to log the user out. If handled
+	 * successfully, the connection will be terminated from the server.
+	 */
 	void quit();
-	
+
 	// post-login commands
-	
-	
+
 	// :SET [ NICKNAME| STATUS | PERSONAL_MESSAGE | DISPLAY_PIC ]: <value>;
-	
-	/** Sends a request to the server to set the user's nickname
-	 * @param nickname 
-	 * 		The desired nickname*/
+
+	/**
+	 * Sends a request to the server to set the user's nickname
+	 * 
+	 * @param nickname
+	 *            The desired nickname
+	 */
 	void setNickname(String nickname);
-	
-	/** Sends a request to the server to set the user's availability
-	 * @param status 
-	 * 		The desired status*/
+
+	/**
+	 * Sends a request to the server to set the user's availability
+	 * 
+	 * @param status
+	 *            The desired status
+	 */
 	void setStatus(String status);
-	
-	
-	/** Sends a request to the server to set the user's personal message
-	 * @param personalmessage 
-	 * 		The desired personal message*/
+
+	/**
+	 * Sends a request to the server to set the user's personal message
+	 * 
+	 * @param personalmessage
+	 *            The desired personal message
+	 */
 	void setPersonalMessage(String personalmessage);
-	
+
 	// figure this one out later... Does it take a string?
-	
-	/** Sends a request to the server to set the user's display picture
+
+	/**
+	 * Sends a request to the server to set the user's display picture
+	 * 
 	 * @param displayPicture
-	 *		the display picture */
+	 *            the display picture
+	 */
 	void setDisplayPicture(String displayPicture);
-	
+
 	// :SET ends
-	
-	// :GET { NICKNAME| STATUS | PERSONAL_MESSAGE | DISPLAY_PIC }: <user>{,<user>};
-	
-	/** Sends a request to the server to return a list of attributes, about a list of users
+
+	// :GET { NICKNAME| STATUS | PERSONAL_MESSAGE | DISPLAY_PIC }:
+	// <user>{,<user>};
+
+	/**
+	 * Sends a request to the server to return a list of attributes, about a
+	 * list of users
+	 * 
 	 * @param attributes
-	 * 		An arrayList of attributes to request from the server
+	 *            An arrayList of attributes to request from the server
 	 * @param users
-	 * 		An arrayList of users to fetch the attributes of from the server */
+	 *            An arrayList of users to fetch the attributes of from the
+	 *            server
+	 */
 	void getAttributes(ArrayList<String> attributes, ArrayList<String> users);
-	
-	/** Sends a request to the server to return the nicknames of a list of users
+
+	/**
+	 * Sends a request to the server to return the nicknames of a list of users
+	 * 
 	 * @param userList
-	 * 		  A list of one or more users, separated by commas */
+	 *            A list of one or more users, separated by commas
+	 */
 	void getNickname(String userList);
-	
-	/** Sends a request to the server to return the statuses of a list of users
+
+	/**
+	 * Sends a request to the server to return the statuses of a list of users
+	 * 
 	 * @param userList
-	 * 		  A list of one or more users, separated by commas */
+	 *            A list of one or more users, separated by commas
+	 */
 	void getStatus(String userList);
-	
-	
-	/** Sends a request to the server to return the personal messages of a list of users
+
+	/**
+	 * Sends a request to the server to return the personal messages of a list
+	 * of users
+	 * 
 	 * @param userList
-	 * 		  A list of one or more users, separated by commas */
+	 *            A list of one or more users, separated by commas
+	 */
 	void getPersonalMessage(String userList);
-	
-	/** Sends a request to the server to return the display pictures of a list of users
+
+	/**
+	 * Sends a request to the server to return the display pictures of a list of
+	 * users
+	 * 
 	 * @param userList
-	 * 		  A list of one or more users, separated by commas */
+	 *            A list of one or more users, separated by commas
+	 */
 	void getDisplayPicture(String userList);
-	
-	
+
 	// :GET ends
-	
+
 	// :FRIENDLIST:;
-	
-	/** Sends a request to the server to return the user's friend list*/
+
+	/** Sends a request to the server to return the user's friend list */
 	void friendlist();
-	
-   // :ROOM [ CREATE {PUBLIC} | INVITE | JOIN | LEAVE | USERS ]: {<roomid> | <user>};
-	
-	/** Sends a request to the server to create a new group chat 
+
+	// :ROOM [ CREATE {PUBLIC} | INVITE | JOIN | LEAVE | USERS ]: {<roomid> |
+	// <user>};
+
+	/**
+	 * Sends a request to the server to create a new group chat
+	 * 
 	 * @param publicchat
-	 * 		if true, then any user has permission to join
-	 * 		if false, only those invited to the chat has permission to join */
+	 *            if true, then any user has permission to join if false, only
+	 *            those invited to the chat has permission to join
+	 */
 	void createGroupChat(Boolean publicchat);
-	
-	
-	/** sends a request to the server to invite a user to a group chat
-	 * @param roomid 
-	 * 		the id of the room to give the user permission to join the group chat of
+
+	/**
+	 * sends a request to the server to invite a user to a group chat
+	 * 
+	 * @param roomid
+	 *            the id of the room to give the user permission to join the
+	 *            group chat of
 	 * @param user
-	 * 		the user to give permission to join the group chat */
+	 *            the user to give permission to join the group chat
+	 */
 	void invite(String roomid, String user);
-	
-	
-	/** send a request to the server to join a group chat 
+
+	/**
+	 * send a request to the server to join a group chat
+	 * 
 	 * @param roomid
-	 * 		the id of the room to join*/
+	 *            the id of the room to join
+	 */
 	void join(String roomid);
-	
-	
-	/** sends a request to the server to leave a group chat
-	 * @param roomid 
-	 * 		the id of the room to leave the group chat of */
+
+	/**
+	 * sends a request to the server to leave a group chat
+	 * 
+	 * @param roomid
+	 *            the id of the room to leave the group chat of
+	 */
 	void leave(String roomid);
-	
-	/** sends a request to the server to return a list of users currently in a group chat
+
+	/**
+	 * sends a request to the server to return a list of users currently in a
+	 * group chat
+	 * 
 	 * @param roomid
-	 * 		the id of the room to get the list of users from */
+	 *            the id of the room to get the list of users from
+	 */
 	void roomusers(String roomid);
-	
+
 	// :ROOM ends
-	
+
 	// :MESSAGE: <roomid> <message>;
-	
-	/** sends a request to the server to send a message to a group chat
+
+	/**
+	 * sends a request to the server to send a message to a group chat
+	 * 
 	 * @param roomid
-	 * 		the ID of the room
+	 *            the ID of the room
 	 * @param message
-	 * 		the message to send to the room */
+	 *            the message to send to the room
+	 */
 	void groupmessage(String roomid, String message);
-	
-	
+
 	// :FRIEND [ ADD | BLOCK | UNBLOCK | ACCEPT | DECLINE | DELETE ]: <target>;
-	
-	
-	/** sends a request to the server to add a friend
+
+	/**
+	 * sends a request to the server to add a friend
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to add */
+	 *            The e-mail address of the user to add
+	 */
 	void add(String friend);
-	
-	/** sends a request to the server to block a friend
+
+	/**
+	 * sends a request to the server to block a friend
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to block */
+	 *            The e-mail address of the user to block
+	 */
 	void block(String friend);
-	
-	/** sends a request to the server to unblock a friend
+
+	/**
+	 * sends a request to the server to unblock a friend
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to unblock */
+	 *            The e-mail address of the user to unblock
+	 */
 	void unblock(String friend);
-	
-	/** sends a request to the server to accept the friend request of a user
+
+	/**
+	 * sends a request to the server to accept the friend request of a user
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to accept the request from */
+	 *            The e-mail address of the user to accept the request from
+	 */
 	void accept(String friend);
-	
-	/** sends a request to the server to decline the friend request of a user
+
+	/**
+	 * sends a request to the server to decline the friend request of a user
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to decline the request from */
+	 *            The e-mail address of the user to decline the request from
+	 */
 	void decline(String friend);
-	
-	/** sends a request to the server to delete a friend
+
+	/**
+	 * sends a request to the server to delete a friend
+	 * 
 	 * @param friend
-	 * 		The e-mail address of the user to delete */
+	 *            The e-mail address of the user to delete
+	 */
 	void delete(String friend);
-	
+
 	// end :FRIEND
-	
+
 	// :LOGOUT:;
-	
-	/** Send a request to the server to log out, but retain a connection to the server */
+
+	/**
+	 * Send a request to the server to log out, but retain a connection to the
+	 * server
+	 */
 	void logout();
-	
+
 }
