40694524b5052f2b59dcb189e0d1e358af3077e7
0800902m <0800902m@306c0d56-3788-c67b-985d-2422301581a3>
2010-12-17 14:38:48 +0000

    excessive committing

--- TP3/trunk/src/client/net/networkReader.java
+++ TP3/trunk/src/client/net/networkReader.java
@@ -7,16 +7,21 @@ import util.CommandBuffer;
 public class networkReader implements Runnable {
 	
 	
+	private CommandBuffer buffer;
+	private BufferedReader reader;
+
 	/** Reads commands off the network and performs calls the necessary method */
 	public networkReader(BufferedReader reader, CommandBuffer buffer) {
 		
-		
+		this.buffer = buffer;
+		this.reader = reader;
 		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+
+		
 		
 	}
 
--- TP3/trunk/src/client/net/networkWriter.java
+++ TP3/trunk/src/client/net/networkWriter.java
@@ -7,15 +7,24 @@ import util.CommandBuffer;
  * Reads commands (formatted appropriately) to send to the server off the
  * CommandBuffer and sends to the server
  */
+
 public class networkWriter implements Runnable {
 
+	private CommandBuffer buffer;
+	private PrintWriter writer;
+
 	public networkWriter(PrintWriter writer, CommandBuffer buffer) {
+		
+		this.buffer = buffer;
+		this. writer = writer;
 
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
 
