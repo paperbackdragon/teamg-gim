58109828039272c607ca5a5f9175d73831b41d01
0800902m <0800902m@306c0d56-3788-c67b-985d-2422301581a3>
2010-12-21 04:04:03 +0000

    going to bed... did i change anything? :S

--- TP3/trunk/src/client/ui/ClientModel.java
+++ TP3/trunk/src/client/ui/ClientModel.java
@@ -9,27 +9,26 @@ public class ClientModel {
 	private ClientConnection outLink;
 	private ServerConnection inLink;
 	private LinkedList<String[]> newRoomList;// = new LinkedList<String[]>();
-	
+
 	// buddy list
 	private String[] onlinefriends;
 	private String[] offlinefriends;
 	private String[] blockedfriends;
-	
-	
-	//CONSTRUCTOR
+
+	// CONSTRUCTOR
 	public ClientModel() {
 		inLink = new ServerConnection();
 		outLink = new ClientConnection(inLink);
-		
+
 		newRoomList = new LinkedList<String[]>();
 	}
-	
-	//ACCESSORS
-	
+
+	// ACCESSORS
+
 	public LinkedList<String[]> getRoomList() {
 		return newRoomList;
 	}
-	
+
 	public String[] getOnlinefriends() {
 		return onlinefriends;
 	}
@@ -53,85 +52,81 @@ public class ClientModel {
 	public void setBlockedfriends(String[] blockedfriends) {
 		this.blockedfriends = blockedfriends;
 	}
-	
-	//MESSAGES TO SERVER
-	
+
+	// MESSAGES TO SERVER
+
 	// Connection stuff
-	
+
 	public void authenticate(String email, char[] pwd) {
 		outLink.authenticate(email, pwd);
 	}
-	
+
 	public void register(String email, char[] pwd) {
 		outLink.register(email, pwd);
 	}
-	
+
 	public void logout() {
 		outLink.logout();
 	}
-	
+
 	public void quit() {
 		outLink.logout();
 		// TODO clean up instead of this:
 		System.exit(0);
 	}
-	
-	//End: connection stuff
-	
-	
+
+	// End: connection stuff
+
 	// ROOM stuff
-	
+
 	public void createRoom(Boolean group, String[] contacts) {
 		newRoomList.add(contacts);
-		if(group)
+		if (group)
 			outLink.createGroupChat();
 		else
 			outLink.createSingleChat(null);// TODO make this not null
 	}
-	
+
 	public void invite(String roomid, String user) {
 		outLink.invite(roomid, user);
 	}
-	
+
 	public void join(String roomid) {
 		outLink.join(roomid);
 	}
-	
+
 	public void leave(String roomid) {
 		outLink.leave(roomid);
 	}
-	
+
 	public void users(String roomid) {
 		outLink.roomusers(roomid);
 	}
-	
+
 	public void type(String roomid) {
 		outLink.type(roomid);
 	}
-	
-	// end: ROOM stuff 
-	
-	
-	
+
+	// end: ROOM stuff
+
 	// Friends list stuff
-	
+
 	public void addfriend(String user) {
 		outLink.add(user);
 	}
-	
+
 	public void blockfriend(String user) {
 		outLink.block(user);
 	}
-	
+
 	public void unblockfriend(String user) {
 		outLink.unblock(user);
 	}
-	
+
 	public void removefriend(String user) {
 		outLink.delete(user);
 	}
-	
-	//end: Friends list stuff
-	
-	
+
+	// end: Friends list stuff
+
 }
