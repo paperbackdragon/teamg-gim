3ae48ac435cc1fabfa12a9148b66a904e8ea4dc9
0800902m <0800902m@306c0d56-3788-c67b-985d-2422301581a3>
2010-12-31 17:46:35 +0000

    i'm making a panel to be used to select contacts from a list (check boxes), and return the check boxes.

---
+++ TP3/trunk/src/client/ui/SelectContactsPanel.java
@@ -0,0 +1,19 @@
+package client.ui;
+
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
+
+public class SelectContactsPanel {
+	
+	JCheckBox checkbox;
+	ArrayList<String> selected;
+	
+	public SelectContactsPanel(String[] friendlist) {
+		
+		
+		
+		
+	}
+
+}
