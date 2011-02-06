fffc4c45fa1e2f23a9cc3493767fc739b86e237f
cyblob@gmail.com <cyblob@gmail.com@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-06 16:24:59 +0000

    Display pictures now work

--- TP3/trunk/src/client/ui/ContactPanel.java
+++ TP3/trunk/src/client/ui/ContactPanel.java
@@ -2,20 +2,18 @@ package client.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedList;
 
 import javax.imageio.ImageIO;
@@ -49,7 +47,7 @@ import client.ValueChangedListener;
 public class ContactPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JButton add, del, chat, group, b;
+	private JButton add, del, chat, group;
 	private JList contactList;
 	protected JScrollPane scrollPane;
 	private Model model = Model.getInstance();
@@ -60,7 +58,6 @@ public class ContactPanel extends JPanel {
 	 * Default constructor
 	 */
 	public ContactPanel() {
-
 
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -95,7 +92,7 @@ public class ContactPanel extends JPanel {
 
 				name = new EditableJLabel(Html.escape(self.getNickname()));
 				message = new EditableJLabel(Html.escape(self.getPersonalMessage()));
-				status = new ListJLabel(new String[] {"Online", "Away", "Busy"}, 0);
+				status = new ListJLabel(new String[] { "Online", "Away", "Busy" }, 0);
 
 				// Create a listener for the editableJLabels
 				ValueChangedListener valueListener = new ValueChangedListener() {
@@ -107,7 +104,7 @@ public class ContactPanel extends JPanel {
 						} else if (source.equals(message)) {
 							self.setPersonalMessage(value);
 							model.getServer().setPersonalMessage(value);
-						} else if(source.equals(status)) {
+						} else if (source.equals(status)) {
 							model.getServer().setStatus(status.getValue());
 						}
 					}
@@ -149,60 +146,82 @@ public class ContactPanel extends JPanel {
 		}
 
 		public PersonalInfo() {
-			
+
 			ChoosePic select = new ChoosePic();
 			setLayout(new BorderLayout(5, 0));
-
 			ImageIcon icon = self.getDisplayPic(64, 64);
-			//JLabel iconLabel = new JLabel(icon);
-			JButton b = new JButton(icon);
+			final JButton b = new JButton(icon);
 			b.addActionListener(select);
 			b.setPreferredSize(new Dimension(64, 64));
-			//iconLabel.add(b);
+
+			self.addUserChangedListener(new UserChangedListener() {
+				@Override
+				public void statusChanged() {
+				}
+
+				@Override
+				public void personalMessageChanged() {
+				}
+
+				@Override
+				public void nicknameChanged() {
+				}
+
+				@Override
+				public void displayPicChanged() {
+					b.setIcon(self.getDisplayPic(64, 64));
+				}
+
+				@Override
+				public void changed() {
+				}
+			});
+
 			add(b, BorderLayout.WEST);
 			add(new OwnInformation(), BorderLayout.CENTER);
 		}
 
 	}
+
 	// User selects picture, in progess....
-	//TODO: Make this do useful stuff
+	// TODO: Make this do useful stuff
 	class ChoosePic implements ActionListener {
+
 		public void actionPerformed(ActionEvent e) {
-			 FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG file", "jpg", "jpeg");
-			 JFileChooser chooser = new JFileChooser();
-			 chooser.addChoosableFileFilter(filter);
-			
-				int option = chooser.showOpenDialog(getParent());
-		        if (option == JFileChooser.APPROVE_OPTION) {
-		        	//String PicName = ...
-		        	//self.setDisplayPic(PicName);	
-		        	
-		        	File file = chooser.getSelectedFile();
-		        	String path = file.getAbsolutePath();
-		        	
-		        	
-		        	/** http://www.programcreek.com/2009/02/java-convert-image-to-byte-array-convert-byte-array-to-image/ */
-		    		ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
-		    		try {
-						BufferedImage img=ImageIO.read(new File(path));
-						ImageIO.write(img, "jpg", baos);
-						baos.flush();
-						
-						String base64String=Base64.encodeToString(baos.toByteArray(), false);
-						baos.close();
-						
-						
-						// I give up... i'll let you do it, james :|
-						//self.setDisplayPic(base64String);
-						
-						
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG file", "jpg", "jpeg");
+			JFileChooser chooser = new JFileChooser();
+			chooser.addChoosableFileFilter(filter);
+
+			int option = chooser.showOpenDialog(getParent());
+
+			if (option == JFileChooser.APPROVE_OPTION) {
+				try {
+					File resized = new File(model.getPath() + "/display_picutre.jpg");
+					BufferedImage originalImage = ImageIO.read(chooser.getSelectedFile());
+					int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+
+					BufferedImage resizedImage = new BufferedImage(128, 128, type);
+					Graphics2D g = resizedImage.createGraphics();
+					g.drawImage(originalImage, 0, 0, 128, 128, null);
+					g.dispose();
+
+					ImageIO.write(resizedImage, "jpg", resized);
+
+					FileInputStream in = new FileInputStream(resized);
+					int c, count = 0;
+					byte[] data = new byte[in.available()];
+
+					while ((c = in.read()) != -1) {
+						data[count++] = (byte) c;
 					}
 
-		        	
-		        	
+					String displayPic = Base64.encodeToString(data, false);
+					self.setDisplayPic(displayPic);
+					model.getServer().setDisplayPicture(displayPic);
+
+				} catch (IOException e1) {
+				}
+
 			}
 		}
 	}
@@ -263,7 +282,7 @@ public class ContactPanel extends JPanel {
 			}
 
 			model.getFriendList().addFriendListChangedListener(new FriendListChangedListener() {
-				
+
 				@Override
 				public void stateChanged() {
 					for (Object o : listModel.toArray()) {
@@ -278,7 +297,7 @@ public class ContactPanel extends JPanel {
 						listModel.addElement(u);
 						u.addUserChangedListener(listener);
 					}
-					
+
 					list.setSelectedIndices(indices);
 
 					onlineLabel.setText("<html><b>Online Contacts (" + model.getFriendList().getOnlineUsers().size()
@@ -316,16 +335,20 @@ public class ContactPanel extends JPanel {
 		public ButtonPanel() {
 			setLayout(new GridLayout(1, 4, 5, 5));
 			add = new JButton();
-			add.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/add.png").getImage().getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
-			
+			add.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/add.png").getImage().getScaledInstance(
+					32, 20, Image.SCALE_SMOOTH)));
+
 			del = new JButton();
-			del.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/remove.png").getImage().getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
-			
+			del.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/remove.png").getImage()
+					.getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
+
 			chat = new JButton();
-			chat.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/chat.png").getImage().getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
-			
+			chat.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/chat.png").getImage().getScaledInstance(
+					32, 20, Image.SCALE_SMOOTH)));
+
 			group = new JButton();
-			group.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/group.png").getImage().getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
+			group.setIcon(new ImageIcon(new ImageIcon(model.getPath() + "/icons/group.png").getImage()
+					.getScaledInstance(32, 20, Image.SCALE_SMOOTH)));
 
 			ButtonListener buttonListener = new ButtonListener();
 			add.addActionListener(buttonListener);
@@ -373,47 +396,41 @@ public class ContactPanel extends JPanel {
 			} else if (e.getSource().equals(del) && getSelectedContacts().size() > 0) {
 				SwingUtilities.invokeLater(new Runnable() {
 					public void run() {
-			
+
 						LinkedList<User> selectedUsers = getSelectedContacts();
 						String users = "";
 						for (User u : selectedUsers) {
 							users += u.getEmail() + " ";
 						}
 
-
 						Object[] options = { "Yes", "No", };
-						int n = JOptionPane
-								.showOptionDialog(
-										null,
-										"Are you sure you want to remove: "
-												+ users
-												+ " from your contacts?",
-										"remove friends", JOptionPane.YES_NO_CANCEL_OPTION,
-										JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-						
-						if(n == 0) {
+						int n = JOptionPane.showOptionDialog(null, "Are you sure you want to remove: " + users
+								+ " from your contacts?", "remove friends", JOptionPane.YES_NO_CANCEL_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+
+						if (n == 0) {
 							for (User u : selectedUsers) {
 								model.getServer().delete(u.getEmail());
 								model.getFriendList().removeUser(u);
 							}
 						}
-						
+
 					}
 				});
 
 			} else if (e.getSource().equals(chat) && getSelectedContacts().size() > 0) {
-				
+
 				LinkedList<User> selectedUsers = getSelectedContacts();
-				
+
 				// Check if there's already a chat open with this user
 				int find = GimClient.findRoom(selectedUsers.getFirst());
-				
+
 				if (find == -1)
 					model.createRoom(getSelectedContacts().getFirst());
 				else
 					GimClient.getWindow(selectedUsers.getFirst()).setVisible(true);
-				
-			} else if (e.getSource().equals(group)  && getSelectedContacts().size() > 0 ) {
+
+			} else if (e.getSource().equals(group) && getSelectedContacts().size() > 0) {
 				model.createRoom(getSelectedContacts());
 			}
 		}
@@ -424,7 +441,7 @@ public class ContactPanel extends JPanel {
 		public void mousePressed(MouseEvent e) {
 
 			LinkedList<User> selectedUsers = getSelectedContacts();
-			
+
 			if (e.getClickCount() == 2) {
 				int find = GimClient.findRoom(selectedUsers.getFirst());
 
