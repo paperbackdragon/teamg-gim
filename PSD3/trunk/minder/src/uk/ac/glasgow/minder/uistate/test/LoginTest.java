10498f4cd59511f3965ebe0cb956f79a4b3bd2ce
heather.hb <heather.hb@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-14 17:28:22 +0000

    Added CreateUserTest and LoginTest, the first of which is mostly implemented.

---
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/LoginTest.java
@@ -0,0 +1,25 @@
+package uk.ac.glasgow.minder.uistate.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.glasgow.minder.recipient.impl.RecipientStoreImpl;
+import uk.ac.glasgow.minder.uistate.impl.UIStateImpl;
+
+public class LoginTest {
+	
+	private RecipientStoreImpl rs;
+	private UIStateImpl s;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
