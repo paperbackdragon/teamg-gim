43889a5d9ec7e920bce07347a169f67b6c4590f1
heather.hb <heather.hb@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-15 15:13:34 +0000

    Most of AddUserToML tests completed. One unfinished, not sure how to do it.

--- PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/AddUserToML.java
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/AddUserToML.java
@@ -3,6 +3,7 @@ package uk.ac.glasgow.minder.uistate.test;
 import java.io.File;
 import java.util.Set;
 
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
 import junit.framework.Assert;
@@ -17,6 +18,7 @@ import uk.ac.glasgow.minder.recipient.Recipient;
 import uk.ac.glasgow.minder.recipient.User;
 import uk.ac.glasgow.minder.recipient.impl.MailingList;
 import uk.ac.glasgow.minder.recipient.impl.RecipientStoreImpl;
+import uk.ac.glasgow.minder.recipient.impl.UserImpl;
 import uk.ac.glasgow.minder.uistate.impl.UIStateImpl;
 
 public class AddUserToML {
@@ -48,40 +50,77 @@ public class AddUserToML {
 		s.addUserToMailingList("Frank Newton", "Level 4");
 		
 		Set<Recipient> recipients = s.searchRecipients("Level 4");
-		//TODO: Doesn't work, println shows too many recipients
-		System.out.println(recipients);
-		Set<User> u = null;
-		for(Recipient r : recipients) {
-			u = ((MailingList) r).getMembers();
-			System.out.println(u);
-		}
+		Object[] recipAry = recipients.toArray();
+		Set<User> u = ((MailingList) recipAry[0]).getMembers();
 		
-		//TODO: Does addUserToMailingList allow users not in the system to be added?
-		Assert.assertEquals(0, u);
+		Assert.assertEquals(0, u.size());
 	}
 
 	@Test
 	public void noUserSpecified() {
+		s.createMailingList("Level 4");
+		s.addUserToMailingList("", "Level 4");
 		
+		Set<Recipient> recipients = s.searchRecipients("Level 4");
+		Object[] recipAry = recipients.toArray();
+		Set<User> u = ((MailingList) recipAry[0]).getMembers();
+		
+		Assert.assertEquals(0, u.size());
 	}
 	
 	@Test
 	public void userAlreadyInML() {
+		s.createMailingList("Level 4");
+		try {
+			s.createUser("Mark Jones", "mark", "pineapple", new InternetAddress("mark@gmail.com"), Privilege.RECIPIENT);
+		} catch (AddressException e) {}
+		s.addUserToMailingList("mark", "Level 4");
+		s.addUserToMailingList("mark", "Level 4");
 		
+		Set<Recipient> recipients = s.searchRecipients("Level 4");
+		Object[] recipAry = recipients.toArray();
+		Set<User> u = ((MailingList) recipAry[0]).getMembers();
+		
+		//TODO: does this overwrite users in mailing lists? does it matter?
+		Assert.assertEquals(1, u.size());
 	}
 	
 	@Test
 	public void MLDoesNotExist() {
+		try {
+			s.createUser("John Smith", "John", "banana", new InternetAddress("jsmith@gmail.com"), Privilege.RECIPIENT);
+		} catch (AddressException e) {}
+		s.addUserToMailingList("John", "Level 7");
 		
+		Set<Recipient> recipients = s.searchRecipients("Level 7");
+		
+		Assert.assertEquals(0, recipients.size());
 	}
 	
 	@Test
 	public void noMLSpecified() {
+		try {
+			s.createUser("John Smith", "John", "banana", new InternetAddress("jsmith@gmail.com"), Privilege.RECIPIENT);
+		} catch (AddressException e) {}
 		
+		s.addUserToMailingList("John", "");
+		
+		//TODO: How to test whether John was added to a mailing list?
+		//Assert.
 	}
 	
 	@Test
 	public void correct() {
+		s.createMailingList("Level 4");
+		try {
+			s.createUser("John Smith", "John", "banana", new InternetAddress("jsmith@gmail.com"), Privilege.RECIPIENT);
+		} catch (AddressException e) {}
+		s.addUserToMailingList("John", "Level 4");
 		
+		Set<Recipient> recipients = s.searchRecipients("Level 4");
+		Object[] recipAry = recipients.toArray();
+		Set<User> u = ((MailingList) recipAry[0]).getMembers();
+		
+		Assert.assertEquals(1, u.size());
 	}
 }
