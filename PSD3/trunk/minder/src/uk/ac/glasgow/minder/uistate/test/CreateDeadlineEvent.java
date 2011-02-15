c252a41282a1d6d8cde29df0a5578a3c53a96eda
gordon.hugh.martin@gmail.com <gordon.hugh.martin@gmail.com@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-15 14:39:29 +0000

    method stubs

---
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/CreateDeadlineEvent.java
@@ -0,0 +1,69 @@
+package uk.ac.glasgow.minder.uistate.test;
+
+
+import java.io.File;
+
+import javax.mail.internet.InternetAddress;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.glasgow.minder.event.impl.Controller;
+import uk.ac.glasgow.minder.recipient.Privilege;
+import uk.ac.glasgow.minder.recipient.impl.RecipientStoreImpl;
+import uk.ac.glasgow.minder.uistate.impl.UIStateImpl;
+
+public class CreateDeadlineEvent {
+	
+	private RecipientStoreImpl rs;
+	private UIStateImpl s;
+
+	@Before
+	public void setUp() throws Exception {
+		rs = new RecipientStoreImpl();
+		rs.addUser("Administrator", "admin", "monkey",
+		new InternetAddress("monkey.me@glasgow.ac.uk"), Privilege.ADMINISTRATOR);
+		
+		s = new UIStateImpl(rs, new Controller(rs));
+		s.login("admin", "monkey");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		File target = new File("mailing.obj");
+		target.delete();
+		target = new File("users.obj");
+		target.delete();
+		
+	}
+	
+	@Test
+	public void dateInvalid() {
+		
+		
+		
+	}
+	
+	@Test
+	public void dateNotProvided() {
+		
+	}
+	
+	@Test
+	public void alreadyHasADeadline() {
+		
+	}
+	
+	@Test
+	public void courseNotProvided() {
+		
+	}
+	
+	@Test
+	public void invalidInput() {
+		
+	}
+
+}
