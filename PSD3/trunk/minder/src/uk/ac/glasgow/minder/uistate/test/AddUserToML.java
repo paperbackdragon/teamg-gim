ae632e3e9d9ba1f67092e2925dd31667e6018ed1
heather.hb <heather.hb@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-15 01:50:19 +0000

    AddUserToML added to, possible bug in code found.

--- PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/AddUserToML.java
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/AddUserToML.java
@@ -1,8 +1,11 @@
 package uk.ac.glasgow.minder.uistate.test;
 
 import java.io.File;
+import java.util.Set;
 
 import javax.mail.internet.InternetAddress;
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +13,9 @@ import org.junit.Test;
 
 import uk.ac.glasgow.minder.event.impl.Controller;
 import uk.ac.glasgow.minder.recipient.Privilege;
+import uk.ac.glasgow.minder.recipient.Recipient;
+import uk.ac.glasgow.minder.recipient.User;
+import uk.ac.glasgow.minder.recipient.impl.MailingList;
 import uk.ac.glasgow.minder.recipient.impl.RecipientStoreImpl;
 import uk.ac.glasgow.minder.uistate.impl.UIStateImpl;
 
@@ -38,7 +44,20 @@ public class AddUserToML {
 	
 	@Test
 	public void userDoesntExist() {
+		s.createMailingList("Level 4");
+		s.addUserToMailingList("Frank Newton", "Level 4");
 		
+		Set<Recipient> recipients = s.searchRecipients("Level 4");
+		//TODO: Doesn't work, println shows too many recipients
+		System.out.println(recipients);
+		Set<User> u = null;
+		for(Recipient r : recipients) {
+			u = ((MailingList) r).getMembers();
+			System.out.println(u);
+		}
+		
+		//TODO: Does addUserToMailingList allow users not in the system to be added?
+		Assert.assertEquals(0, u);
 	}
 
 	@Test
