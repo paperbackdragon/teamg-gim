e474dc817885096cbe744a0374062bf36c7a5917
gordon.hugh.martin@gmail.com <gordon.hugh.martin@gmail.com@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-15 14:30:48 +0000

    added createlectureevent class

---
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/CreateLectureEvent.java
@@ -0,0 +1,17 @@
+package uk.ac.glasgow.minder.uistate.test;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class CreateLectureEvent {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
