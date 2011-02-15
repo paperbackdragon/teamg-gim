46a6d1995182842b35f41c2a0cbee0354ada2b5a
gordon.hugh.martin@gmail.com <gordon.hugh.martin@gmail.com@306c0d56-3788-c67b-985d-2422301581a3>
2011-02-15 15:43:04 +0000

    done some test cases, not ran them them

--- PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/CreateDeadlineEvent.java
+++ PSD3/trunk/minder/src/uk/ac/glasgow/minder/uistate/test/CreateDeadlineEvent.java
@@ -2,8 +2,13 @@ package uk.ac.glasgow.minder.uistate.test;
 
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.mail.internet.InternetAddress;
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -41,29 +46,72 @@ public class CreateDeadlineEvent {
 	
 	@Test
 	public void dateInvalid() {
+		s.createMailingList("Level 4");
+		Calendar cal = new GregorianCalendar(01, 01, 1800);
+		Date date = cal.getTime();
+		
+		s.createDeadlineEvent(date, "Exercise 1", "History");
 		
 		
+		// ?? am i searching on the right thing, james?
+		Assert.assertEquals(0, s.searchEvents("Exercise 1").size());
 		
 	}
 	
 	@Test
 	public void dateNotProvided() {
 		
+		s.createMailingList("Level 4");
+		s.createDeadlineEvent(null, "Exercise 1", "History");
+		
+		// ?? am i searching on the right thing, james?
+		Assert.assertEquals(0, s.searchEvents("Exercise 1").size());
+		
 	}
 	
 	@Test
 	public void alreadyHasADeadline() {
 		
+		s.createMailingList("Level 4");
+		
+		Calendar cal = new GregorianCalendar(25, 11, 2010);
+		Date date = cal.getTime();
+		
+		s.createDeadlineEvent(date, "Exercise 2", "Maths");
+		s.createDeadlineEvent(date, "Exercise 2", "Maths");
+		
+		// ?? am i searching on the right thing, james?
+		Assert.assertEquals(1, s.searchEvents("Exercise 2").size());
+		
 	}
 	
 	@Test
 	public void courseNotProvided() {
+		s.createMailingList("Level 4");
+		Calendar cal = new GregorianCalendar(25, 11, 2010);
+		Date date = cal.getTime();
+		
+		s.createDeadlineEvent(date, "Exercise 1", null);
+		
+		Assert.assertEquals(0, s.searchEvents("Exercise 4").size());
+		
 		
 	}
 	
 	@Test
-	public void invalidInput() {
+	public void validInput() {
+		
+		s.createMailingList("Level 4");
+		Calendar cal = new GregorianCalendar(25, 11, 2010);
+		Date date = cal.getTime();
+		
+		s.createDeadlineEvent(date, "Exercise 3", "Maths");
+		
+		
+		
+		
 		
 	}
+	
 
 }
